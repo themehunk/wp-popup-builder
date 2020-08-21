b78aceeb12305b3f3640666c4446cd24ca59337b
rahul verma <rahulvarma722@gmail.com>
2020-08-21 17:53:59 +0530

    icon section added

---
+++ inc/wppb-help.php
@@ -0,0 +1,13 @@
+<section class="wppb-pro-panel-help" data-tab-active='help' data-tab-group="pro-to-free">
+    <div>
+        <h1>Lead Form Builder</h1>
+        <p class="help-txt">Lead form builder is a easy form builder and a lead generator. It will take your business to a next level. This plugin contains drag and drop field ordering, add/remove field option, form size setting, admin/user setting. You can create unlimited number of responsive forms and use it anywhere on your site. (Page, Post, Widgets).</p>
+    </div>
+    <div>
+        <p class="help-txt">For any type of difficulty please check plugin documentation</p>
+        <a class="link-btn-front" href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Documentation</a>
+        <p class="help-txt">For any type of difficulty please check plugin documentation</p>
+        <a class="link-btn-front" href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Support Forum</a>
+    </div>
+
+</section>
--- inc/wppb-pro-popup.php
+++ inc/wppb-pro-popup.php
@@ -1,29 +1,29 @@
-<div class="wppb-pro-feature-panel">
+<section class="wppb-pro-feature-panel" data-tab-active='view-free-to-pro' data-tab-group="pro-to-free">
     <div class="wppb-pro-panel-one">
         <h1>Build Best Forms With Lead Form Builder Pro !</h1>
-        <a href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Buy Now - 29$</a>
+        <a class="link-btn-front" href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Buy Now - 29$</a>
         <div class="wppb-divider"></div>
-        <img src="<?php echo WPPB_URL.'img/wppb-pro-plugin.png'; ?>" alt="">
+        <img class="pro-feature-img" src="<?php echo WPPB_URL.'img/wppb-pro-plugin.png'; ?>" alt="">
         <p>Generate more leads means more clients, just take your forms to next level</p>
         <p class="wppb-join">Join our over 5,000+ users!</p>
         <h1>Powerful Advanced Features</h1>
         <div class="wppb-divider"></div>
         <ul class="wppb-feature">
             <li>
-                <span class="dashicons dashicons-plus-alt"></span>
-                <p>Section Model</p>
+                <span class="dashicons dashicons-facebook-alt"></span>
+                <p>Icon Module</p>
+            </li>
+            <li>
+                <span class="dashicons dashicons-menu-alt3"></span>
+                <p>Add Multiple Section</p>
             </li>
             <li>
                 <span class="dashicons dashicons-plus-alt"></span>
                 <p>Section Model</p>
             </li>
             <li>
-                <span class="dashicons dashicons-plus-alt"></span>
-                <p>Section Model</p>
-            </li>
-            <li>
-                <span class="dashicons dashicons-plus-alt"></span>
-                <p>Section Model</p>
+                <span class="dashicons dashicons-text"></span>
+                <p>Add Icon List</p>
             </li>
             <li>
                 <span class="dashicons dashicons-plus-alt"></span>
@@ -35,6 +35,6 @@
             </li>
 
         </ul>
-        <a href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Buy Now - 29$</a>
+        <a class="link-btn-front" href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Buy Now - 29$</a>
     </div>
-</div>
+</section>
