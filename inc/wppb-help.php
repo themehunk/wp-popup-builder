2baab7a9b34927f1232d245f6bbb0503a02541fe
rahul verma <rahulvarma722@gmail.com>
2020-08-21 19:02:14 +0530

    popup section added

--- inc/wppb-help.php
+++ inc/wppb-help.php
@@ -1,13 +1,13 @@
 <section class="wppb-pro-panel-help" data-tab-active='help' data-tab-group="pro-to-free">
     <div>
-        <h1>Lead Form Builder</h1>
-        <p class="help-txt">Lead form builder is a easy form builder and a lead generator. It will take your business to a next level. This plugin contains drag and drop field ordering, add/remove field option, form size setting, admin/user setting. You can create unlimited number of responsive forms and use it anywhere on your site. (Page, Post, Widgets).</p>
+        <h1>DRAG & DROP POPUP BUILDER</h1>
+        <p class="help-txt">WordPress popup builder is a great plugin for your business websites. Easiest to use with beautiful Pre-built Popup templates & a powerful drag and drop editor. It allows you to create and design lightbox popups to show them in your site Pages, Post and Widget areas ( Using Shortcodes or Without Shortcodes ). Its Drag and drop interface will help you in designing popups with ease. You can simply design your own popups by adding a background, choosing colors, images, fonts, and styling. WP popup builder has a real time editor to see the frequent changes on screen. It is fastest Popup builder comes with incredible Features and functionality. This will easily convert website visitors into subscribers and customers.</p>
     </div>
     <div>
         <p class="help-txt">For any type of difficulty please check plugin documentation</p>
-        <a class="link-btn-front" href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Documentation</a>
+        <a class="link-btn-front" href="https://themehunk.com/docs/wp-popup-builder/" target="_blank">Documentation</a>
         <p class="help-txt">For any type of difficulty please check plugin documentation</p>
-        <a class="link-btn-front" href="https://themehunk.com/wp-popup-builder-pro/" target="_blank">Support Forum</a>
+        <a class="link-btn-front" href="https://www.themehunk.com/support/" target="_blank">Support Forum</a>
     </div>
 
 </section>
