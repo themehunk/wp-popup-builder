22b204f620cfc272f98caece10c789beab5776ed
upcominginfo <rahulvarma722@gmail.com>
2021-07-29 16:10:37 +0530

    redme text file changed

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: ThemeHunk
 Author URI: : https://www.themehunk.com/
 Tags: popup,wp popup, popup maker, popup builder, popups,marketing
 Requires at least: 5.5
-Tested up to: 5.7.1
-Stable tag: 1.1.14
+Tested up to: 5.8
+Stable tag: 1.2.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -102,6 +102,10 @@ Just upload the `wp-popup-builder.zip` to the `/wp-content/plugins/` directory, 
 
 
 == Changelog ==
+= 1.2.0 =
+* shortcode addon added 
+* Preview in popup setting page added
+* 2 column popup issue fix 
 
 = 1.1.14 =
 * notify Updated.
--- wp-popup-builder.php
+++ wp-popup-builder.php
@@ -1,14 +1,14 @@
 <?php
 if (!defined('ABSPATH')) exit;
-if (!function_exists('wppb_loaded_pro')) {
 /*
   Plugin Name: WP Popup Builder
   Description: WP Popup Builder is a powerfull tool to create amazing popup form for your site. Its drag and drop feature helps to create form in very easy step without having knowledge of coding. And also you can easily design and edit your form using easy to use interface. It has ready to use "Pre Built Popup" to give a quick start for your form, Also you can create your own design by choosing three different "Layouts" available. Images, Heading, Text, Button and even external form can be added to it.
-  Version: 1.1.14
+  Version: 1.2.0
   Author: ThemeHunk
   Author URI: http://www.themehunk.com/
   Text Domain: wppb
  */
+if (!function_exists('wppb_loaded_pro')) {
   define('WPPB_URL', plugin_dir_url(__FILE__));
   define('WPPB_PATH', plugin_dir_path(__FILE__));
   define("WPPB_PAGE_URL", admin_url('admin.php?page=wppb'));
