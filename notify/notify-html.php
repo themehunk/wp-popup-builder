936a871056c3fe61c7a621dfe990390def1066df
harshv251 <harshv251@gmail.com>
2021-12-16 12:41:22 +0530

    amaz-store banner added

--- notify/notify-html.php
+++ notify/notify-html.php
@@ -2,35 +2,35 @@
   <div class="notice th-notice-slide-wrapper" style="display:<?php echo $display; ?>;">
  <div class="th-notice-slide-wrap">
     <?php 
-    if($theme !== 'jot-shop'){ ?>
+    if($theme !== 'amaz-store'){ ?>
 
                 <div class="thunk-notice">
                     <div class="thunk-review-thumbnail">
-                        <a href="https://themehunk.com/product/jot-shop/"
+                        <a href="https://themehunk.com/product/amaz-store/"
                            target="_blank">
-                            <img src="<?php echo WPPB_URL; ?>notify/assets/images/jot-shop.png"
-                                 alt="Jot Shop - New Free WooCommerce Theme"/>
+                            <img src="<?php echo WPPB_URL; ?>notify/assets/images/amaz-store.png"
+                                 alt="Amaz Store - Free Shopping WordPress Theme"/>
                         </a>
                     </div>
                     <div class="thunk-notice-text">
-                        <h3>Jot Shop - Best Free WooCommerce Theme</h3>  
+                        <h3>Amaz Store - Free Shopping WordPress Theme</h3>  
                         <a href="?notice-disable=1"  class="notice-dismiss dashicons dashicons-dismiss dashicons-dismiss-icon">
                             </a>
-                         <p>Jot Shop is a Free WooCommerce theme for creating clean & professional estores for your business. Theme is integrated with WooCommerce, Th Variation Swatches plugin and Th Advanced Product Search plugin which will help you in designing a perfect Online Store.
+                         <p>Amaz Store is a Free WooCommerce theme for creating clean online stores for your business. Ready to import pre made demos which makes your web designing more easy.
                         </p>
                         <ul class="thunk-review-ul">
                             <li class="show-mor-message">
-                                <a href="https://themehunk.com/product/jot-shop/#tabfix"
+                                <a href="https://themehunk.com/product/amaz-store/#tabfix"
                                    target="_blank">
                                     <span class="dashicons dashicons-desktop"></span>
                                     Live Demo
                                 </a>
                             </li>
                             <li class="free-download-message">
-                                <a href="https://themehunk.com/product/jot-shop/"
+                                <a href="https://themehunk.com/product/amaz-store/"
                                    target="_blank">
                                     <span class="dashicons dashicons-external"></span>
-                              Check Detail
+                              Know More
                                 </a>
                             </li>
                         </ul>
